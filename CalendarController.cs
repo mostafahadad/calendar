a9271180171a8691ca17217bbda4681348b78f32
mostafahadad <142294519+mostafahadad@users.noreply.github.com>
2023-12-05 21:26:46 +0100

    Add files via upload

---
+++ CalendarController.cs
@@ -0,0 +1,39 @@
+﻿using Calender.Data.Entities;
+using Calender.Services.Calendars;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calender.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalendarController : ControllerBase
+    {
+        private readonly ICalendarService _calendarService;
+
+        public CalendarController(ICalendarService calendarService)
+        {
+            _calendarService = calendarService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Calendar>> GetCalendar(int id)
+        {
+            try
+            {
+                return Ok(await _calendarService.GetByIdAsync(id));
+            }
+            catch(Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Calendar>> PostCalendar(Calendar calendar)
+        {
+            var newCalendar = await _calendarService.AddAsync(calendar);
+
+            return CreatedAtAction("GetCalendar", new { id = newCalendar.Id }, newCalendar);
+        }
+    }
+}
---
+++ EventController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calender.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+    }
+}
